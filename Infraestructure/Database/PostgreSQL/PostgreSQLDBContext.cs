eaa3730d0323c2d054280c7ff1033dc7a9d56ac0
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-01-15 21:53:37 -0300

    chore: db context postgres with factory

---
+++ Infraestructure/Database/PostgreSQL/PostgreSQLDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.DigitalAccount;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Database.PostgreSQL
+{
+    public class PostgreSQLDBContext : DbContext
+    {
+        public PostgreSQLDBContext(DbContextOptions<PostgreSQLDBContext> options) : base(options) { }
+
+        public DbSet<DigitalAccount> DigitalAccounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
