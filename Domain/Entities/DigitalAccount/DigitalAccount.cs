ae3b8a387eed48840cb1b4b25d7d277b43db5cbd
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-01-15 21:58:10 -0300

    feat: create entity and repository digital account initial configs

---
+++ Domain/Entities/DigitalAccount/DigitalAccount.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.DigitalAccount
+{
+    public class DigitalAccount(double Amount) : BaseEntity
+    {
+        [Required]
+        public double Amount { get; set; } = Amount;
+    }
+}
---
+++ Infraestructure/Repositories/DigitalAccountRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.DigitalAccount;
+using Infraestructure.Database.PostgreSQL;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+
+namespace Infraestructure.Repositories
+{
+    public class DigitalAccountRepository : BaseRepository<DigitalAccount>
+    {
+        private readonly PostgreSQLDBContext _context;
+        private readonly DbSet<DigitalAccount> _dbSet;
+
+        public DigitalAccountRepository(PostgreSQLDBContext context) : base(context)
+        {
+            _context = context;
+            _dbSet = _context.Set<DigitalAccount>();
+        }
+    }
+}
